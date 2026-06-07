6d778dae41cf835c6662b7824a09dbd03f6fd138
ybbyybyb <05318352256zxzx@gmail.com>
2026-06-07 03:01:42 +0300

    Create vercel.js

---
+++ vercel.js
@@ -0,0 +1,20 @@
+{
+  "rewrites": [
+    {
+      "source": "/pages/market/index",
+      "destination": "/market.html"
+    },
+    {
+      "source": "/pages/Asset/Index",
+      "destination": "/asset.html"
+    },
+    {
+      "source": "/h5",
+      "destination": "/index.html"
+    },
+    {
+      "source": "/pages/My/Index",
+      "destination": "/my.html"
+    }
+  ]
+}
